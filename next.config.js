9ad9bb5f5a2a75b41e26655a33f9744bf608f083
namanrajkhadia <138713305+namanrajkhadia@users.noreply.github.com>
2024-10-07 00:14:19 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,18 @@ module.exports = {
       {
         source: '/api/:path*',
         destination: process.env.NEXT_PUBLIC_API_URL + '/api/:path*',
+      webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  },
+};
       },
     ]
   },
