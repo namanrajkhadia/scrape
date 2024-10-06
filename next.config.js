ffa437bcf263ada169fd004480393d56c9d82457
namanrajkhadia <138713305+namanrajkhadia@users.noreply.github.com>
2024-10-06 22:50:04 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,11 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    appDir: true
-  },
   env: {
-    FLASK_API_URL: process.env.FLASK_API_URL,
+    FLASK_API_URL: process.env.FLASK_API_URL || 'https://crawls.in',
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
   },
 }
 
