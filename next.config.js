03cb02a111c9ad3b289869627a802b206a83ab06
namanrajkhadia <138713305+namanrajkhadia@users.noreply.github.com>
2024-10-07 07:47:08 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,21 +1,7 @@
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: process.env.NEXT_PUBLIC_API_URL + '/api/:path*',
-      },
-    ];
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-      };
-    }
-    return config;
-  },
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+module.exports = nextConfig
